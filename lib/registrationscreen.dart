f233e28bc3b9876f1a12d2760ad53b1daf0900fd
Alexandre Ramos <78726166+Alexsantossp71@users.noreply.github.com>
2022-02-02 12:09:53 -0300

    primeiro - welcome - splah - iniciada autentica;áo

--- lib/registrationscreen.dart
+++ lib/registrationscreen.dart
@@ -7,7 +7,7 @@ class RegistrationScreen extends StatefulWidget {
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 
-class _RegistrationScreenState extends State<RegistrationScreen> {
+class _RegistrationScreenState extends   State<RegistrationScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
